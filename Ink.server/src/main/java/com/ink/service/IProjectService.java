6fb5eea683d40cd019d86083cc264af1315cad37
Carlos <458074992@qq.com>
2019-06-02 21:27:00 +0800

    project

---
+++ Ink.server/src/main/java/com/ink/service/IProjectService.java
@@ -0,0 +1,7 @@
+package com.ink.service.Impl;
+
+import java.util.ArrayList;
+
+public interface IProjectService {
+    public ArrayList getProjectByUerId(Integer userId);
+}
---
+++ Ink.server/src/main/java/com/ink/service/Impl/projectServiceImpl.java
@@ -0,0 +1,7 @@
+package com.ink.service.Impl;
+
+/**
+ * @author Created by carlos
+ */
+public class projectServiceImpl {
+}
