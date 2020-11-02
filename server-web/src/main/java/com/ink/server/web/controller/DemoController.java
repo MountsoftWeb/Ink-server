3cfb9645f2ef7d98030272bb1b478f7328bab469
Carlos <458074992@qq.com>
2020-11-02 23:14:16 +0800

    测试

--- server-web/src/main/java/com/ink/server/web/controller/DemoController.java
+++ server-web/src/main/java/com/ink/server/web/controller/DemoController.java
@@ -9,10 +9,8 @@ import com.ink.server.service.DemoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.ArrayList;
 
 
